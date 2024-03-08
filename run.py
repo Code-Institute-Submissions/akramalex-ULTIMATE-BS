35f2729ddcbce946df4e7a934734c88991778217
akramalex <info@quick4you.net>
2024-03-08 12:39:44 +0000

    fixing bug printing ships sometimes 4 sometimes 3

--- run.py
+++ run.py
@@ -220,6 +220,12 @@ def new_game():
     computer_board = Board(size, num_ships, "computer", type="computer")
     player_board = Board(size, num_ships, player_name, type="player")
 
+    # Clearing the ships and guesses lists
+    computer_board.ships = []
+    computer_board.guesses = []
+    player_board.ships = []
+    player_board.guesses = []
+
     play_game(computer_board, player_board)
 
 
